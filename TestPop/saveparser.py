ba5b04e78ba3dce5c1251033a342dfb622cf5ee4
KJH <wogudwkd1233@gmail.com>
2020-11-03 13:42:12 +0900

    if this doesn't work, then I'll shit my pants

--- TestPop/saveparser.py
+++ TestPop/saveparser.py
@@ -52,6 +52,29 @@ def parse_block(block):
 
     return file
 
+def reconstruct(file, t=''):
+    txt = ''
+
+    for f in file:
+        if f:
+            if len(f) == 3 and type(f[0]) != type(list()) and type(f[1]) != type(list()):
+                if type(f[2]) == type(list()):
+                    tail = ''
+                        
+                    if f[2]:
+                        if type(f[2][0]) != type(list()):
+                            tail = ' '.join(f[2])
+                        else:
+                            tail = '\n%s%s' % (reconstruct(f[2], t + '\t'), t)
+
+                    txt += '%s%s %s { %s}\n' % (t, f[0], f[1], tail)
+                else:
+                    txt += '%s%s %s %s\n' % (t, f[0], f[1], f[2])
+            elif len(f) == 2 and type(f[0]) != type(list()):
+                txt += '%s[%s\n%s%s]\n' % (t, f[0], reconstruct(f[1], t + '\t'), t)
+
+    return txt
+
 if __name__ == '__main__':
     names = ['prod_templates', 'prod_instances', 'trade_merchants', 'tech_techs', 'tech_eras', 'build_templates', 'build_slots_active', 'build_slots']
     
@@ -60,6 +83,8 @@ if __name__ == '__main__':
 
         glob_vars = []
         glob_lists = []
+
+        print('Parsing Globals')
 
         start = t.find('\nvariables={')
         end = t.find('\ngame_rules={', start)
@@ -84,9 +109,11 @@ if __name__ == '__main__':
             if lst[0] in names:
                 for var in lst[1]:
                     if not var[1] in chars:
-                        chars[var[1]] = (i, [], [])
+                        chars[var[1]] = (i, [], [], [])
                         i += 1
 
+        print('Parsing Chars')
+        
         start = t.find('\nliving={')
         end = t.find('\ndead_unprunable={', start)
 
@@ -120,88 +147,8 @@ if __name__ == '__main__':
                                 break
                         break
 
-        with open('common\\scripted_effects\\00_sim_char.txt', 'w', encoding='utf-8-sig') as ff:
-            out = ['sim_char = {\n']
-
-            for char in chars:
-                out.append('''\tcreate_character = {
-        save_scope_as = sim_char_%s
-        gender = male
-        trait = character_not_1
-        employer = character:999999
-        faith = character:999999
-        culture = character:999999
-        dynasty = none
-    }\n''' % char)
-
-            for char, data in chars.items():
-                foo = []
-
-                for var in data[1]:
-                    if type(var) == type(tuple()):
-                        if var[1] == 'value':
-                            if int(var[2]) / 1000 > 210000:
-                                foo.append('set_variable = { name = %s value = 0 } ' % var[0])
-                            else:
-                                foo.append('set_variable = { name = %s value = %s } ' % (var[0], round(int(var[2]) / 1000, 3)))
-                        elif var[1] == 'char':
-                            foo.append('set_variable = { name = %s value = scope:sim_char_%s } ' % (var[0], var[2]))
-                        elif var[1] == 'prov':
-                            foo.append('set_variable = { name = %s value = province:%s } ' % (var[0], var[2]))
-                        elif var[1] == 'boolean':
-                            foo.append('set_variable = { name = %s } ' % var[0])
-                        elif var[1] == 'lt':
-                            foo.append('set_variable = { name = %s value = title:%s } ' % (var[0], var[2]))
-                    else:
-                        foo.append('set_variable = { name = %s } ' % var)
-                for lst in data[2]:
-                    for var in lst[1]:
-                        if var[0] == 'char':
-                            foo.append('add_to_variable_list = { name = %s target = scope:sim_char_%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'prov':
-                            foo.append('add_to_variable_list = { name = %s target = province:%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'lt':
-                            foo.append('add_to_variable_list = { name = %s target = title:%s } ' % (lst[0], var[1]))
-            
-                out.append('\tscope:sim_char_%s = { %s}\n' % (char, ''.join(foo)))
-
-            out.append('}')
-            
-            ff.write(''.join(out))
-
-        with open('common\\scripted_effects\\00_sim_glob.txt', 'w', encoding='utf-8-sig') as ff:
-            out = ['sim_glob = {\n']
-
-            for var in glob_vars:
-                if type(var) == type(tuple()):
-                    if var[1] == 'value':
-                        if int(var[2]) / 1000 > 210000:
-                            out.append('\tset_global_variable = { name = %s value = 0 }\n' % var[0])
-                        else:
-                            out.append('\tset_global_variable = { name = %s value = %s }\n' % (var[0], round(int(var[2]) / 1000, 3)))
-                    elif var[1] == 'char' and var[2] in chars:
-                        out.append('\tset_global_variable = { name = %s value = scope:sim_char_%s }\n' % (var[0], var[2]))
-                    elif var[1] == 'prov':
-                        out.append('\tset_global_variable = { name = %s value = province:%s }\n' % (var[0], var[2]))
-                    elif var[1] == 'boolean':
-                        out.append('\tset_global_variable = { name = %s }\n' % var[0])
-                    elif var[1] == 'lt':
-                        out.append('\tset_global_variable = { name = %s value = title:%s }\n' % (var[0], var[2]))
-                else:
-                    out.append('\t\tset_global_variable = { name = %s }\n' % var)
-            for lst in glob_lists:
-                for var in lst[1]:
-                    if var[0] == 'char':
-                        out.append('\tadd_to_global_variable_list = { name = %s target = scope:sim_char_%s }\n' % (lst[0], var[1]))
-                    elif var[0] == 'prov':
-                        out.append('\tadd_to_global_variable_list = { name = %s target = province:%s }\n' % (lst[0], var[1]))
-                    elif var[0] == 'lt':
-                        out.append('\tadd_to_global_variable_list = { name = %s target = title:%s }\n' % (lst[0], var[1]))
-
-            out.append('}')
-
-            ff.write(''.join(out))
-
+        print('Parsing Provs')
+        
         provs = []
         
         start = t.find('\nprovinces={')
@@ -234,44 +181,8 @@ if __name__ == '__main__':
 
             provs.append(foo)
 
-        with open('common\\scripted_effects\\00_sim_prov.txt', 'w', encoding='utf-8-sig') as ff:
-            out = ['sim_prov = {\n']
-
-            for data in provs:
-                foo = []
-
-                for var in data[1]:
-                    if type(var) == type(tuple()):
-                        if var[1] == 'value':
-                            if int(var[2]) / 1000 > 210000:
-                                foo.append('set_variable = { name = %s value = 0 } ' % var[0])
-                            else:
-                                foo.append('set_variable = { name = %s value = %s } ' % (var[0], round(int(var[2]) / 1000, 3)))
-                        elif var[1] == 'char':
-                            foo.append('set_variable = { name = %s value = scope:sim_char_%s } ' % (var[0], var[2]))
-                        elif var[1] == 'prov':
-                            foo.append('set_variable = { name = %s value = province:%s } ' % (var[0], var[2]))
-                        elif var[1] == 'boolean':
-                            foo.append('set_variable = { name = %s } ' % var[0])
-                        elif var[1] == 'lt':
-                            foo.append('set_variable = { name = %s value = title:%s } ' % (var[0], var[2]))
-                    else:
-                        foo.append('set_variable = { name = %s } ' % var)
-                for lst in data[2]:
-                    for var in lst[1]:
-                        if var[0] == 'char':
-                            foo.append('add_to_variable_list = { name = %s target = scope:sim_char_%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'prov':
-                            foo.append('add_to_variable_list = { name = %s target = province:%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'lt':
-                            foo.append('add_to_variable_list = { name = %s target = title:%s } ' % (lst[0], var[1]))
-
-                out.append('\tprovince:%s = { %s}\n' % (data[0], ''.join(foo)))
-
-            out.append('}')
-            
-            ff.write(''.join(out))
-
+        print('Parsing Titles')
+        
         titles = []
         
         start = t.find('\nlanded_titles={')
@@ -315,40 +226,121 @@ if __name__ == '__main__':
 
             titles.append(foo)
 
-        with open('common\\scripted_effects\\00_sim_titles.txt', 'w', encoding='utf-8-sig') as ff:
-            out = ['sim_titles = {\n']
+        print('Writing Events')
+        
+        event = '''save_data.0%s = {
+	type = empty
+	hidden = yes
+	
+	immediate = { 
+%s
+	}
+	
+	option = {
+            trigger_event = {
+                id = save_data.0%s
+            }
+	}
+}
+'''
+        arhat = '\t\tcharacter:999999 = { set_global_variable = { name = arhat value = this } every_courtier_or_guest = { limit = { is_character = yes } death = natural } every_councillor = { limit = { is_character = yes } death = natural } }'
+        create_character = '\t\tcreate_character = { save_scope_as = save_data_char_%s gender = male trait = character_not_1 employer = global_var:arhat faith = global_var:arhat.faith culture = global_var:arhat.culture dynasty = none }\n'
 
-            for data in titles:
-                foo = []
+        def helper(form, var, chars):
+            if type(var) == type(tuple()):
+                if var[1] == 'value':
+                    if int(var[2]) / 1000 > 210000:
+                        return '%s = { name = %s %s = 0 }\n' % (form[0], var[0], form[1])
+                    else:
+                        return '%s = { name = %s %s = %s }\n' % (form[0], var[0], form[1], round(int(var[2]) / 1000, 3))
+                elif var[1] == 'char' and var[2] in chars:
+                    return '%s = { name = %s %s = scope:save_data_char_%s }\n' % (form[0], var[0], form[1], var[2])
+                elif var[1] == 'prov':
+                    return '%s = { name = %s %s = province:%s }\n' % (form[0], var[0], form[1], var[2])
+                elif var[1] == 'boolean':
+                    return '%s = { name = %s }\n' % (form[0], var[0])
+                elif var[1] == 'lt':
+                    return '%s = { name = %s %s = title:%s }\n' % (form[0], var[0], form[1], var[2])
+            else:
+                return '%s = { name = %s }\n' % (form[0], var)
+        
+        with open('events\\save_data.txt', 'w', encoding='utf-8-sig') as ff:
+            print('Writing Arhat Event')
+            
+            out = ['namespace = save_data\n\n', event % (1, arhat, 2)]
+
+            print('Writing Char Creation Event')
+
+            out.append(event % (2, ''.join([create_character % char for char in chars]), 3))
+
+            print('Writing Char Data Event')
+
+            outout = []
+
+            for char, data in chars.items():
+                outout.append('\t\tscope:save_data_char_%s = {\n' % char)
 
                 for var in data[1]:
-                    if type(var) == type(tuple()):
-                        if var[1] == 'value':
-                            if int(var[2]) / 1000 > 210000:
-                                foo.append('set_variable = { name = %s value = 0 } ' % var[0])
-                            else:
-                                foo.append('set_variable = { name = %s value = %s } ' % (var[0], round(int(var[2]) / 1000, 3)))
-                        elif var[1] == 'char':
-                            foo.append('set_variable = { name = %s value = scope:sim_char_%s } ' % (var[0], var[2]))
-                        elif var[1] == 'prov':
-                            foo.append('set_variable = { name = %s value = province:%s } ' % (var[0], var[2]))
-                        elif var[1] == 'boolean':
-                            foo.append('set_variable = { name = %s } ' % var[0])
-                        elif var[1] == 'lt':
-                            foo.append('set_variable = { name = %s value = title:%s } ' % (var[0], var[2]))
-                    else:
-                        foo.append('set_variable = { name = %s } ' % var)
+                    outout.append(helper(('\t\t\tset_variable', 'value'), var, chars))
                 for lst in data[2]:
                     for var in lst[1]:
-                        if var[0] == 'char':
-                            foo.append('add_to_variable_list = { name = %s target = scope:sim_char_%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'prov':
-                            foo.append('add_to_variable_list = { name = %s target = province:%s } ' % (lst[0], var[1]))
-                        elif var[0] == 'lt':
-                            foo.append('add_to_variable_list = { name = %s target = title:%s } ' % (lst[0], var[1]))
+                        outout.append(helper(('\t\t\tadd_to_variable_list', 'target'), (lst[0], var[0], var[1]), chars))
 
-                out.append('\ttitle:%s = { %s}\n' % (data[0], ''.join(foo)))
+                outout.append('\t\t}\n')
 
-            out.append('}')
+            out.append(event % (3, ''.join([item for item in outout if item]), 4))
+
+            print('Writing Global Data Event')
+
+            outout = []
+
+            for var in glob_vars:
+                outout.append(helper(('\t\tset_global_variable', 'value'), var, chars))
+            for lst in glob_lists:
+                for var in lst[1]:
+                    outout.append(helper(('\t\tadd_to_global_variable_list', 'target'), (lst[0], var[0], var[1]), chars))
+                    
+            out.append(event % (4, ''.join([item for item in outout if item]), 5))
+
+            print('Writing Prov Data Event')
+
+            outout = []
+
+            for data in provs:
+                outout.append('\t\tprovince:%s = {\n' % data[0])
+
+                for var in data[1]:
+                    outout.append(helper(('\t\t\tset_variable', 'value'), var, chars))
+                for lst in data[2]:
+                    for var in lst[1]:
+                        outout.append(helper(('\t\t\tadd_to_variable_list', 'target'), (lst[0], var[0], var[1]), chars))
+
+                outout.append('\t\t}\n')
+
+            out.append(event % (5, ''.join([item for item in outout if item]), 6))
+
+            print('Writing Title Data Event')
+
+            outout = []
+
+            for data in titles:
+                outout.append('\t\ttitle:%s = {\n' % data[0])
+
+                for var in data[1]:
+                    outout.append(helper(('\t\t\tset_variable', 'value'), var, chars))
+                for lst in data[2]:
+                    for var in lst[1]:
+                        outout.append(helper(('\t\t\tadd_to_variable_list', 'target'), (lst[0], var[0], var[1]), chars))
+
+                outout.append('\t\t}\n')
+
+            out.append(event % (5, ''.join([item for item in outout if item]), 6))
+
+            print('Parsing and Reconstructing the Events')
+
+            out[1] = reconstruct(parse_block(out[1]))
+            out[2] = reconstruct(parse_block(out[2]))
+
+            print('Joining the Events')
             
             ff.write(''.join(out))
